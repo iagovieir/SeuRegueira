adbb7dcf611b7406283b1134becf3940d9d80370
Iago Vieira <iago.unijp@gmail.com>
2023-08-24 11:20:44 -0300

    testando background 4.0

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function Close(){
     var divPoupup = document.getElementById("popup")
     divPoupup.style.display = "none";
 }
+function instagram(){
+    window.location.href = 'https://www.instagram.com/seuregueira/'
+}
+
+function twitter(){
+    window.location.href = 'https://twitter.com/seuregueirazn'
+}
+function facebook(){
+    window.location.href = ''
+}
+function googlemaps(){
+    window.location.href = 'https://goo.gl/maps/DYjYyvzdxfJUzdi48'
+}
+function mapswaze(){
+    window.location.href = 'https://ul.waze.com/ul?place=Ej1SLiBSZWd1ZWlyYSBDb3N0YSAtIFJvc2FyaW5obywgUmVjaWZlIC0gUEUsIDUyMDQxLTA1MCwgQnJhemlsIi4qLAoUChIJ27Ju4lYYqwcR9E3qCasZq6sSFAoSCc9AtC9XGKsHEcv2dae8-CFA&ll=-8.03368190%2C-34.89836030&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location'
+}
+function wsp(){
+    window.location.href = 'https://api.whatsapp.com/send?phone=5581981881144&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20das%20redes%20sociais,%20gostaria%20de%20comprar%20meu%20ingresso%20do%20@seuregueira'
+}
+function acesosvip(){
+    window.location.href = 'https://www.sympla.com.br/regueira-exclusive__2127462?token=ec76b47eb4a764dd56e0bee8807f8b6f'
+}
+
