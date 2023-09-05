028ada5c5026fa47a7fb2597a511f7c0821d2c1d
Iago Vieira <iago.unijp@gmail.com>
2023-09-05 09:32:23 -0300

    atualizações solicitadas, mas não remunerada

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function Close(){
     var divPoupup = document.getElementById("popup")
     divPoupup.style.display = "none";
 }
+
 function instagram(){
     window.location.href = 'https://www.instagram.com/seuregueira/'
 }
-
 function twitter(){
     window.location.href = 'https://twitter.com/seuregueirazn'
 }
@@ -21,12 +21,6 @@ function googlemaps(){
 }
 function mapswaze(){
     window.location.href = 'https://ul.waze.com/ul?place=Ej1SLiBSZWd1ZWlyYSBDb3N0YSAtIFJvc2FyaW5obywgUmVjaWZlIC0gUEUsIDUyMDQxLTA1MCwgQnJhemlsIi4qLAoUChIJ27Ju4lYYqwcR9E3qCasZq6sSFAoSCc9AtC9XGKsHEcv2dae8-CFA&ll=-8.03368190%2C-34.89836030&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location'
-}
-function wsp(){
-    window.location.href = 'https://api.whatsapp.com/send?phone=5581981881144&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20das%20redes%20sociais,%20gostaria%20de%20comprar%20meu%20ingresso%20do%20@seuregueira'
-}
-function acesosvip(){
-    window.location.href = 'https://www.sympla.com.br/produtor/seuregueira'
 }
 function uber(){
     window.location.href = 'https://m.uber.com/ul/?action=setPickup&client_id=56wiWyloFq7Iosr6uxSS1Wc_VOHNLSLd&pickup=my_location&dropoff[formatted_address]=Rua%20Regueira%20Costa%20-%20Rosarinho%2C%20Recife%20-%20PE%2C%20Brasil&dropoff[latitude]=-8.033682&dropoff[longitude]=-34.898360'
